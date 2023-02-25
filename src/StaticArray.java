02876d2db17d9aee85ac24faf1ed4093845e00a1
hs <skagotjd456@gmail.com>
2023-02-25 21:39:21 +0900

    고정 배열 구현

---
+++ src/StaticArray.java
@@ -0,0 +1,117 @@
+import java.util.Iterator;
+
+// 고정 크기 배열
+class StaticArray<E> implements Iterable<E> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private final int capacity;
+    private final Object[] elements;
+    private int size = 0;
+
+    // 생성자
+    public StaticArray() {
+        this.capacity = DEFAULT_CAPACITY;
+        elements = new Object[capacity];
+    }
+
+    public StaticArray(int capacity) {
+        if (capacity >= 0) {
+            this.capacity = capacity;
+            elements = new Object[capacity];
+        } else {
+            throw new IllegalArgumentException("Illegal argument: " +
+                    capacity);
+        }
+    }
+
+    // 메서드
+    public boolean add(E e) {
+        checkIndex(size, capacity);
+        elements[size++] = e;
+        return true;
+    }
+
+    public boolean remove(Object o) {
+        int index = indexOf(o);
+        if (index == -1) {
+            return false;
+        }
+        remove(index);
+        return true;
+    }
+
+    public E remove(int index) {
+        checkIndex(index, size);
+        @SuppressWarnings("unchecked") E oldValue = (E) elements[index];
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        size--;
+        return oldValue;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+    public int indexOf(Object o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elements[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elements[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E get(int index) {
+        checkIndex(index, size);
+        return (E) elements[index];
+    }
+
+    public void clear() {
+        size = 0;
+    }
+
+    private void checkIndex(int index, int length) {
+        if (index < 0) {
+            throw new IndexOutOfBoundsException("Index should be up 0");
+        } else if (index >= length) {
+            throw new IndexOutOfBoundsException("Index should be within size");
+        }
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < size && elements[currentIndex] != null;
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public E next() {
+                return (E) elements[currentIndex++];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+}
---
+++ test/StaticArrayTest.java
@@ -0,0 +1,105 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StaticArrayTest {
+
+    @Test
+    void testAdd() {
+        StaticArray<Integer> array = new StaticArray<>(2);
+        assertTrue(array.add(1));
+        assertTrue(array.add(2));
+        Throwable exception = assertThrows(IndexOutOfBoundsException.class, () -> {
+            array.add(3);
+        });
+
+        assertEquals("Index should be within size", exception.getMessage());
+    }
+
+    @Test
+    void testRemove() {
+        StaticArray<String> array = new StaticArray<>(2);
+        array.add("apple");
+        array.add("banana");
+        assertTrue(array.remove("banana"));
+        assertFalse(array.remove("orange"));
+        assertEquals(1, array.size());
+        assertEquals("apple", array.get(0));
+    }
+
+    @Test
+    void testRemoveAtIndex() {
+        StaticArray<String> array = new StaticArray<>(2);
+        array.add("apple");
+        array.add("banana");
+        assertEquals("banana", array.remove(1));
+        assertEquals("apple", array.get(0));
+        assertEquals(1, array.size());
+
+        Throwable exception = assertThrows(IndexOutOfBoundsException.class, () -> {
+            array.remove(10);
+        });
+        assertEquals("Index should be within size", exception.getMessage());
+    }
+
+    @Test
+    void testSize() {
+        StaticArray<Double> array = new StaticArray<>(4);
+        array.add(1.1);
+        array.add(2.2);
+        array.add(3.3);
+        assertEquals(3, array.size());
+        array.remove(1);
+        assertEquals(2, array.size());
+    }
+
+    @Test
+    void testContains() {
+        StaticArray<String> array = new StaticArray<>(3);
+        array.add("apple");
+        array.add("banana");
+        assertTrue(array.contains("apple"));
+        assertFalse(array.contains("orange"));
+    }
+
+    @Test
+    void testIndexOf() {
+        StaticArray<String> array = new StaticArray<>(3);
+        array.add("apple");
+        array.add("banana");
+        assertEquals(1, array.indexOf("banana"));
+        assertEquals(-1, array.indexOf("orange"));
+    }
+
+    @Test
+    void testGet() {
+        StaticArray<String> array = new StaticArray<>(3);
+        array.add("apple");
+        array.add("banana");
+        assertEquals("apple", array.get(0));
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            array.get(2);
+        });
+    }
+
+    @Test
+    void testClear() {
+        StaticArray<Integer> array = new StaticArray<>(2);
+        array.add(1);
+        array.add(2);
+        array.clear();
+        assertEquals(0, array.size());
+    }
+
+    @Test
+    void testIterator() {
+        StaticArray<Integer> array = new StaticArray<>(2);
+        array.add(1);
+        array.add(2);
+        StringBuilder sb = new StringBuilder();
+        for (int i : array) {
+            sb.append(i);
+        }
+        assertEquals("12", sb.toString());
+    }
+}
